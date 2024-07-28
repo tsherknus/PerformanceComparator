3ac5f078fd622dad76ef491803b65f7b1f7e3ebe
Tyler Sherknus <tylersherknus@gmail.com>
2024-07-28 18:22:10 -0400

    add winners and losers to output

--- PerformanceComparator.py
+++ PerformanceComparator.py
@@ -5,6 +5,8 @@ import datetime
 from AnnualPerformanceComparator import *
 
 output_list = []
+winners_list = []
+loser_list = []
 
 
 def add_to_dict(ticker, benchmark_pct_chg, ticker_pct_increase, first_ticker_candle, last_ticker_candle):
@@ -21,11 +23,19 @@ def add_to_dict(ticker, benchmark_pct_chg, ticker_pct_increase, first_ticker_can
                  'period_end': period_end.strftime('%Y-%m-%d'), 'comparison_period': f"{comparison_period / 365} years",
                  'annual_benchmark_beat_pct': annual_beat_pct}
 
+    if (temp_dict['benchmark_beat'] > 5 and temp_dict['annual_benchmark_beat_pct']
+            and temp_dict['annual_benchmark_beat_pct'] > 0.65):
+        winners_list.append(temp_dict)
+
+    if (temp_dict['benchmark_beat'] < 0 and temp_dict['annual_benchmark_beat_pct']
+            and temp_dict['annual_benchmark_beat_pct'] < 0.35):
+        loser_list.append(temp_dict)
+
     output_list.append(temp_dict)
 
 
-def write_file(benchmark, output_list):
-    with open(benchmark + "_overall.json", "w+") as test:
+def write_file(benchmark, output_list, file_name):
+    with open(benchmark + file_name, "w+") as test:
         test.seek(0)  # <- This is the missing piece
         test.truncate()
         json.dump(output_list, test, indent=4, sort_keys=True)
@@ -74,6 +84,13 @@ annual_perf = {}
 for benchmark in benchmarks:
     annual_perf = annual_benchmark_beat_pct(benchmark)
     overall_performance_comparator(benchmark)
-    new_list = sorted(output_list, key=lambda d: d['benchmark_beat'])
-    write_file(benchmark, new_list)
+    output_list_1 = sorted(output_list, key=lambda d: d['benchmark_beat'])
+    winners_list_1 = sorted(winners_list, key=lambda d: d['benchmark_beat'])
+    loser_list_1 = sorted(loser_list, key=lambda d: d['benchmark_beat'])
+    write_file(benchmark, output_list_1, "_overall.json")
+    write_file(benchmark, winners_list_1, "_winners.json")
+    write_file(benchmark, loser_list_1, "_losers.json")
     output_list = []
+    winners_list = []
+    loser_list = []
+
