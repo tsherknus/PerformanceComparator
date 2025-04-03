675ac2ebf324ee0519fc83d896e3af3a9ce92a8d
Tyler Sherknus <tylersherknus@gmail.com>
2025-04-02 22:03:58 -0400

    * Add comparison and benchmark folders * Output underperform list lists * Clean up output * Handle empty candle lists

--- README.md
+++ README.md
@@ -8,8 +8,9 @@ larger list of stocks.
 ## Instructions
 
 1. Generate the list of <ticker>_candles.json files using the HistoricPriceAggregator project
-2. Delete the temp_candle.json file from the data folder
-3. Copy the <ticker>_candles.json files from the HistoricPriceAggregator into the data folder
-4. Specify the duration in months that the pct increase will be benchmarked over in StocksToBenchmark.py
-5. Specify the benchmark ticker that will be used to measure individual stocks in StocksToBenchmark.py
-6. This project will generate a list of tickers that have outperformed the benchmark
+2. Delete the temp_candle.json file from the benchmark and comparison folder
+3. Copy the <benchmark>_candles.json file from the HistoricPriceAggregator into the benchmark folder
+4. Copy the tickers to compare <ticker>_candles.json files from the HistoricPriceAggregator into the comparison folder
+5. Specify the duration in months that the pct increase will be benchmarked over in StocksToBenchmark.py
+6. Specify the benchmark ticker that will be used to measure individual stocks in StocksToBenchmark.py
+7. This project will generate a list of tickers that have outperformed the benchmark and a list of tickers that have underperformed the benchmark
--- StocksToBenchmark.py
+++ StocksToBenchmark.py
@@ -1,13 +1,16 @@
 import os
 import json
 
-def get_pct_change(ticker, comparison_period):
-    with open("data/" + ticker + "_candles.json") as file:
+def get_pct_change(path_to_candles, comparison_period):
+    with open(path_to_candles) as file:
         candles = json.load(file)
         candle_list = []
 
         for candle in candles:
             candle_list.append(candle)
+
+        if len(candle_list) == 0:
+            return 0
 
         comparison_candles = candle_list[-comparison_period:]
         starting_candle = candles[comparison_candles[0]]['close']
@@ -18,22 +21,32 @@ def get_pct_change(ticker, comparison_period):
 # Enter the period to compare against the benchmark in months
 comparison_period = 120
 # Enter the benchmark ticker to compare against
-benchmark_ticker = "QQQ"
+benchmark_ticker = 'QQQ'
 
-benchmark_pct_change = get_pct_change(benchmark_ticker, comparison_period)
+benchmark_path = 'data/benchmark/' + benchmark_ticker + '_candles.json'
+
+benchmark_pct_change = get_pct_change(benchmark_path, comparison_period)
 print(f'Benchmark PCT Change: {benchmark_pct_change}')
-dir_list = os.listdir("data")
+
+comparison_path = 'data/comparison/'
+
+dir_list = os.listdir(comparison_path)
+
 outperform_list = []
+underperform_list = []
 
 for file_name in dir_list:
-    ticker = file_name.split("_")[0]
-    pct_change = get_pct_change(ticker, comparison_period)
-    if pct_change > benchmark_pct_change:
+    ticker = file_name.split('_')[0]
+    pct_change = get_pct_change(comparison_path+file_name, comparison_period)
+    if pct_change >= benchmark_pct_change:
         outperform_list.append(ticker)
-        print(f'{ticker} PCT Change {pct_change}')
+        # print(f'{ticker} PCT Change {pct_change}')
+    else:
+        underperform_list.append(ticker)
+        # print(f'{ticker} PCT Change {pct_change}')
 
-print(len(dir_list)-1)
-print(len(outperform_list))
+print(f'Outperformers ({len(outperform_list)}):')
 print(outperform_list)
-
+print(f'Underperformers ({len(underperform_list)}):')
+print(underperform_list)
 
